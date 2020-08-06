0ac00faca5cf714caffc204194149f0cb1286ec9
Harry Merritt <harry@merritt.es>
2020-08-06 01:01:00 +0100

    create hash table store using capacity

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -22,6 +22,8 @@ class HashTable:
 
     def __init__(self, capacity):
         # Your code here
+        self.capacity = capacity
+        self.store = [None] * capacity
 
 
     def get_num_slots(self):
