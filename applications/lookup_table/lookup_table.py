349c85d60878d801e54a17d5af87fa4645834f66
Harry Merritt <harry@merritt.es>
2020-08-11 23:24:25 +0100

    complete lookup_table: create and build cache

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,8 @@
-# Your code here
+import math
+import random
 
+# Your code here
+cache = {}
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -15,8 +18,23 @@ def slowfun(x, y):
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    # Check cache
+    if f"{x}{y}" in cache:
+        return cache[f"{x}{y}"]
+    else:
+        v = math.pow(x, y)
+        v = math.factorial(v)
+        v //= (x + y)
+        v %= 982451653
 
+        cache[f"{x}{y}"] = v
+        return v
 
+# Build cache
+print("Building slowfun cache...\n")
+for i in range(2, 14):
+    for k in range(3, 6):
+        cache[f"{i}{k}"] = slowfun(i, k)
 
 # Do not modify below this line!
 
