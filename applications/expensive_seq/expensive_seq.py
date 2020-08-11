bb5c9b1c5c65982c2bcf4f82d7febdd47fac155b
Harry Merritt <harry@merritt.es>
2020-08-11 23:49:26 +0100

    complete expensive_seq: implement memoization

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,20 @@
 # Your code here
+cache = {}
 
 
 def expensive_seq(x, y, z):
     # Your code here
+    if x <= 0:
+        return y + z
+
+    if x > 0:
+        if (x, y, z) in cache:
+            return cache[(x, y, z)]
+        else:
+            # Run calc + add result to cache (memoization)
+            result = expensive_seq(x-1,y+1,z) + expensive_seq(x-2,y+2,z*2) + expensive_seq(x-3,y+3,z*3)
+            cache[(x, y, z)] = result
+            return result
 
 
 
