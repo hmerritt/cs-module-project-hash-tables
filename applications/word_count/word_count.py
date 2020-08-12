b396786a7b0e5b87410f675319443ccbc879ec64
Harry Merritt <harry@merritt.es>
2020-08-12 01:00:55 +0100

    complete word_count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,22 @@
+
+import re
+
 def word_count(s):
     # Your code here
+    output = {}
 
+    words = s.lower().replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').split(" ")
+
+    for w in words:
+        word = w
+        word = re.sub('[\"\:\;\,\.\-\+\=\/\\\|\[\]\{\}\(\)\*\^\&]', '', word)
+        if len(word) > 0:
+            if word not in output:
+                output[word] = 1
+            else:
+                output[word] += 1
+
+    return output
 
 
 if __name__ == "__main__":
