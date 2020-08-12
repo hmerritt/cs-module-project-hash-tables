c9d8cd0fa9d69d9197c26fddb07b2db714bb2254
Harry Merritt <harry@merritt.es>
2020-08-12 01:11:35 +0100

    complete no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,24 @@
 def no_dups(s):
     # Your code here
+    output = ""
+    cache = {}
+    split = s.split(" ")
+
+    for word in split:
+        if len(word) > 0:
+            if word not in cache:
+                output = f"{output} {word}"
+                cache[word] = 1
+            else:
+                cache[word] += 1
+
+    if len(output) > 0:
+        if output[0] == " ":
+            output = output[1:]
+        if output[-1] == " ":
+            output = output[:-1]
+
+    return output
 
 
 
