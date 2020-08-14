4a49b4c7dc90ea43af8a3c8ba916809b20bb930d
Harry Merritt <harry@merritt.es>
2020-08-14 01:57:23 +0100

    complete histogram

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,36 @@
-# Your code here
+import re
+import operator
 
+# Read in all the words in one go
+with open("robin.txt") as f:
+    words_raw = f.read()
+
+def word_count():
+    # Your code here
+    output = {}
+
+    words = words_raw.lower().replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').split(" ")
+
+    for w in words:
+        word = w
+        word = re.sub('[\"\:\;\,\.\-\+\=\/\\\|\[\]\{\}\(\)\*\^\&]', '', word)
+        if len(word) > 0:
+            if word not in output:
+                output[word] = 1
+            else:
+                output[word] += 1
+
+    return output
+
+output = ""
+words_count = word_count()
+
+if len(words_count) > 0:
+    sort = sorted(words_count.items(), key=operator.itemgetter(1))
+    sort.reverse()
+    max = max(len(x) for x in words_count)
+
+    for word in sort:
+        spaces = " " * (max + 4 - len(word[0]))
+        hashes = "#" * word[1]
+        print(f"{word[0]} {spaces} {hashes}")
